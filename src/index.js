735eea11569e61fb28a9a090cbc991850156d8a1
Rumus Bajracharya <rumus.3353@gmail.com>
2022-02-22 16:58:35 +0545

    State in react

--- src/index.js
+++ src/index.js
@@ -2,23 +2,40 @@
 import React from "react";
 import ReactDOM from "react-dom";
 //create app component
-const getButtonText = () => {
-  return { text: "Click me" };
-};
 
-const App = () => {
-  let style = { backgroundColor: "blue", color: "white", borderRadius: "25px" };
-  return (
-    <div>
-      <label className="label" htmlFor="name">
-        Enter your name:
-      </label>
-      <input id="name" type="text"></input>
-      <button type="submit" style={style}>
-        {getButtonText().text}
-      </button>
-    </div>
-  );
-};
+class App extends React.Component {
+  constructor(props) {
+    //intialize super
+    super(props);
+    //setup initial state
+    this.state = { lat: null, test: "yeet", errorMessage: "" };
+    //geolocation api
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        console.log(position);
+        //called set state
+        //set state will call the rerender the component
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => {
+        //handling errors
+        console.error(err);
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+
+    return <div>Loading......</div>;
+  }
+}
 //display app component
 ReactDOM.render(<App />, document.querySelector("#root"));
