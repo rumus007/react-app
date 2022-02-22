eab2bf25a31291ee2e978848752cf73bb7da97d3
Rumus Bajracharya <rumus.3353@gmail.com>
2022-02-22 11:47:07 +0545

    Functional components basics

--- src/index.js
+++ src/index.js
@@ -2,8 +2,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 //create app component
+const getButtonText = () => {
+  return { text: "Click me" };
+};
+
 const App = () => {
-    return <div>Hello world</div>;
+  let style = { backgroundColor: "blue", color: "white", borderRadius: "25px" };
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        Enter your name:
+      </label>
+      <input id="name" type="text"></input>
+      <button type="submit" style={style}>
+        {getButtonText().text}
+      </button>
+    </div>
+  );
 };
 //display app component
 ReactDOM.render(<App />, document.querySelector("#root"));
